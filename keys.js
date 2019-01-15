f275a35a2d874dbe96b4367c01b7763743aefb7d
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-01-15 16:50:02 -0700

    started liri

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.798ddf3c45e54a1a8c1e1f06bc2795ec,
-  secret: process.env.706693bdc1864ae9a70ce647d5042b20 
-}
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,3 +1,20 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
+var artist = "";
+var song = "";
+var movie = "";
+var input = process.argv;
+
+if (input[2] === "concert-this") {
+    console.log(1);
+} else if (input[2] === "spotify-this-song") {
+    console.log(2);
+} else if (input[2] === "movie-this") {
+    console.log(3);
+} else if (input[2] === "do-what-it-says") {
+    console.log(4);
+} else {
+    console.log("I don't understand");
+}
