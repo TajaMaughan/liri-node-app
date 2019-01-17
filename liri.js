f53292ddb71d5af2f32c438392cb8cb2b2cd4a55
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-01-17 11:43:26 -0700

    concert-this working

--- liri.js
+++ liri.js
@@ -1,20 +1,66 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
 var fs = require("fs");
-var artist = "";
-var song = "";
-var movie = "";
+var spotify = new Spotify(keys.spotify);
 var input = process.argv;
 
+
+
+
+
 if (input[2] === "concert-this") {
-    console.log(1);
+    concertThis();
 } else if (input[2] === "spotify-this-song") {
-    console.log(2);
+    spotifyThisSong();
 } else if (input[2] === "movie-this") {
     console.log(3);
 } else if (input[2] === "do-what-it-says") {
     console.log(4);
 } else {
     console.log("I don't understand");
+}
+
+function concertThis() {
+    var artist = process.argv.slice(3).join(" ");
+    console.log(artist);
+    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function (response) {
+        if (!response.data) {
+            console.log("No data returned.")
+        } else {
+            var result = response.data[0];
+            console.log(result.venue.name);
+            console.log(result.venue.city + ", " + result.venue.region + ", " + result.venue.country);
+            console.log(moment(result.datetime).format("MM/DD/YYYY"));
+        }
+    })
+}
+
+function spotifyThisSong() {
+    var song = process.argv.slice(3).join(" ");
+    console.log(song);
+    spotify.search({ type: 'track', query: song, limit: 1}, function(err,data) {
+        if(err) {
+            return console.log('Error occured: ' + err);
+        }
+        var result = data.tracks.items;
+        console.log(result.album);
+    })
+}
+
+function movieThis() {
+    var movie = process.argv.slice(3).join(" ");
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, response) {
+        if (err) {
+            console.log("error");
+        } else {
+
+        }
+    })
 }
