186c1102a6a39e60d5d0d0ce80a83c5e238d5a70
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-01-19 16:08:43 -0700

    finished app

--- liri.js
+++ liri.js
@@ -5,66 +5,86 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
-var input = process.argv;
+var type = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+var divider = "\n\n------------------------------------\n\n";
 
-if (input[2] === "concert-this") {
-    concertThis();
-} else if (input[2] === "spotify-this-song") {
-    spotifyThisSong();
-} else if (input[2] === "movie-this") {
-    movieThis();
-} else if (input[2] === "do-what-it-says") {
-    doWhatItSays();
-} else {
-    console.log("I don't understand");
+function search() {
+    if (type === "concert-this") {
+        concertThis();
+    } else if (type === "spotify-this-song") {
+        spotifyThisSong();
+    } else if (type === "movie-this") {
+        movieThis();
+    } else if (type === "do-what-it-says") {
+        doWhatItSays();
+    } else {
+        console.log("I don't understand");
+    }
 }
 
 function concertThis() {
-    var artist = process.argv.slice(3).join(" ");
+    var artist = term;
     let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(function (response) {
-        //if (err) {
-            //return console.log('Error occured: ' + err);
+            //return console.log("Could not search artist. Try again.");
         //} else {
             var result = response.data[0];
-            console.log("Artist: " + artist);
-            console.log("Venue: " + result.venue.name);
-            console.log("Location: " + result.venue.city + ", " + result.venue.region + ", " + result.venue.country);
-            console.log("Date: " + moment(result.datetime).format("MM/DD/YYYY"));
+            var entry = "\nArtist: " + artist + "\nVenue: " + result.venue.name + "\nLocation: " + result.venue.city + ", " + result.venue.region + ", " + result.venue.country + "\nDate: " + moment(result.datetime).format("MM/DD/YYYY");
+            console.log(entry);
         //}
+        fs.appendFile("log.txt", entry + divider, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("\nEntry logged");
+        })
     })
 }
 
 function spotifyThisSong() {
-    var song = process.argv.slice(3).join(" ");
-    spotify.search({ type: 'track', query: song }, function (err, response) {
+    var song = term;
+    if (song == "") {
+        song = "the sign";
+    }
+    spotify.search({ type: "track", query: song }, function (err, response) {
         if (err) {
-            return console.log('Error occured: ' + err);
+            return console.log("Could not search song. Try again");
         } else {
             var result = response.tracks.items[0];
-            console.log("Artist: " + result.album.artists[0].name);
-            console.log("Song: " + result.name);
-            console.log("Preview URL: " + result.preview_url);
-            console.log("Album: " + result.album.name);
+            var entry = "\nArtist: " + result.album.artists[0].name + "\nSong: " + result.name + "\nPreview URL: " + result.preview_url + "\nAlbum: " + result.album.name;
+            console.log(entry);
         }
+        fs.appendFile("log.txt", entry + divider, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("\nEntry logged");
+        })
     })
 }
 
 function movieThis() {
-    var movie = process.argv.slice(3).join(" ");
+    var movie = term;
+    if (movie == "") {
+        movie = "Mr. Nobody";
+    }
     let queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    axios.get(queryURL).then(
-        function (response) {
+    axios.get(queryURL).then(function (response) {
+        //if (err) {
+            //return console.log("Could not search movie. Try again.")
+        //} else {
             var result = response.data;
-            console.log("Movie: " + result.Title);
-            console.log("Release year: " + result.Year);
-            console.log("IMDB rating: " + result.imdbRating);
-            console.log("Rotten Tomatoes rating: " + result.Ratings[1].Value);
-            console.log("Country: " + result.Country);
-            console.log("Language: " + result.Language);
-            console.log("Plot: " + result.Plot);
-            console.log("Actors: " + result.Actors);
+            var entry = "\nMovie: " + result.Title + "\nRelease year: " + result.Year + "\nIMDB rating: " + result.imdbRating + "\nRotten Tomatoes rating: " + result.Ratings[1].Value + "\nCountry: " + result.Country + "\nLanguage: " + result.Language + "\nPlot: " + result.Plot + "\nActors: " + result.Actors;
+            console.log(entry);
+        //}
+        fs.appendFile("log.txt", entry + divider, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("\nEntry logged");
         })
+    })
 }
 
 function doWhatItSays() {
@@ -72,7 +92,12 @@ function doWhatItSays() {
         if (err) {
             console.log("error");
         } else {
-
+            array = response.split(",");
+            type = array[0];
+            term = array[1];
+            search();
         }
     })
 }
+
+search();
